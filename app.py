95f3ac1bdbfc8e9b6b39012a3650891b58d25842
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-01-28 16:55:07 +0000

    Add application code

---
+++ app.py
@@ -0,0 +1,9 @@
+from download import download_cycles_info
+from plot import plot_map
+from tweeter import tweet
+
+
+def execute():
+    information = download_cycles_info()
+    map_image = plot_map(information)
+    tweet(map_image)
