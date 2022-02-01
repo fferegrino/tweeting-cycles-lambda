74af9fb9dd4e4ecd572c48a875bee7cd812a828a
Antonio Feregrino <antonio.feregrino@gmail.com>
2022-02-01 19:04:58 +0000

    Add terraform

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "3.74.0"
+  constraints = "~> 3.27"
+  hashes = [
+    "h1:y9b9LluBQGrZHURGY1Xmrhb+zQ6qRit3oqFSLijkGe0=",
+    "zh:00767509c13c0d1c7ad6af702c6942e6572aa6d529b40a00baacc0e73faafea2",
+    "zh:03aafdc903ad49c2eda03889f927f44212674c50e475a9c6298850381319eec2",
+    "zh:2de8a6a97b180f909d652f215125aa4683e99db15fcf3b28d62e3d542f875ed6",
+    "zh:3ac29ebc3af99028f4230a79f56606a0c2954b68767bd749b921a76eb4f3bd30",
+    "zh:50add2e2d118a15a644360eabc5a34cec59f2560b491f8fabf9c52ab83ca7b09",
+    "zh:85dd8e81910ab79f841a4a595fdd8ac358fbfe460956144afb0be3d81f91fe10",
+    "zh:895de83d0f0941fde31bfc53fa6b1ea276901f006bec221bbdee4771a04f3693",
+    "zh:a15c9724aac52d1ba5001d2d83e42843099b52b1638ea29d84e20be0f45fa4f1",
+    "zh:c982a64463bd73e9bff2589de214b1de0a571438d9015001f9eae45cfc3a2559",
+    "zh:e9ef973c18078324e43213ea1252c12b9441e566bf054ddfdbff5dd62f3035d9",
+    "zh:f297e705b0f339c8baa27ae70db5df9aa6578adfe1ea3d2ba8edc186512464eb",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/null" {
+  version     = "3.0.0"
+  constraints = "~> 3.0.0"
+  hashes = [
+    "h1:V1tzrSG6t3e7zWvUwRbGbhsWU2Jd/anrJpOl9XM+R/8=",
+    "zh:05fb7eab469324c97e9b73a61d2ece6f91de4e9b493e573bfeda0f2077bc3a4c",
+    "zh:1688aa91885a395c4ae67636d411475d0b831e422e005dcf02eedacaafac3bb4",
+    "zh:24a0b1292e3a474f57c483a7a4512d797e041bc9c2fbaac42fe12e86a7fb5a3c",
+    "zh:2fc951bd0d1b9b23427acc93be09b6909d72871e464088171da60fbee4fdde03",
+    "zh:6db825759425599a326385a68acc6be2d9ba0d7d6ef587191d0cdc6daef9ac63",
+    "zh:85985763d02618993c32c294072cc6ec51f1692b803cb506fcfedca9d40eaec9",
+    "zh:a53186599c57058be1509f904da512342cfdc5d808efdaf02dec15f0f3cb039a",
+    "zh:c2e07b49b6efa676bdc7b00c06333ea1792a983a5720f9e2233db27323d2707c",
+    "zh:cdc8fe1096103cf5374751e2e8408ec4abd2eb67d5a1c5151fe2c7ecfd525bef",
+    "zh:dbdef21df0c012b0d08776f3d4f34eb0f2f229adfde07ff252a119e52c0f65b7",
+  ]
+}
---
+++ terraform/main.tf
@@ -0,0 +1,153 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+
+    null = {
+      version = "~> 3.0.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+
+  backend "s3" {
+    bucket = "feregrino-terraform-states"
+    key    = "lambda-cycles-final"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-1"
+}
+
+data "aws_caller_identity" "current_identity" {}
+
+locals {
+  account_id          = data.aws_caller_identity.current_identity.account_id
+  prefix              = "lambda-cycles-final"
+  ecr_repository_name = "${local.prefix}-image-repo"
+  region              = "eu-west-1"
+  ecr_image_tag       = "latest"
+}
+
+data "aws_secretsmanager_secret" "twitter_secrets" {
+  arn = "arn:aws:secretsmanager:${local.region}:${local.account_id}:secret:lambda/cycles/twitter-2GMvKu"
+}
+
+data "aws_secretsmanager_secret_version" "current_twitter_secrets" {
+  secret_id = data.aws_secretsmanager_secret.twitter_secrets.id
+}
+
+resource "aws_ecr_repository" "lambda_image" {
+  name                 = local.ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "null_resource" "ecr_image" {
+  triggers = {
+    python_file_1 = filemd5("../app.py")
+    python_file_2 = filemd5("../plot.py")
+    python_file_3 = filemd5("../tweeter.py")
+    python_file_4 = filemd5("../download.py")
+    requirements  = filemd5("../requirements.txt")
+    docker_file   = filemd5("../Dockerfile")
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+           aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${local.region}.amazonaws.com
+           docker tag lambda-cycles ${aws_ecr_repository.lambda_image.repository_url}:${local.ecr_image_tag}
+           docker push ${aws_ecr_repository.lambda_image.repository_url}:${local.ecr_image_tag}
+       EOF
+  }
+}
+
+data "aws_ecr_image" "lambda_image" {
+  depends_on = [
+    null_resource.ecr_image
+  ]
+  repository_name = local.ecr_repository_name
+  image_tag       = local.ecr_image_tag
+}
+
+resource "aws_iam_role" "lambda" {
+  name               = "${local.prefix}-lambda-role"
+  assume_role_policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Action": "sts:AssumeRole",
+           "Principal": {
+               "Service": "lambda.amazonaws.com"
+           },
+           "Effect": "Allow"
+       }
+   ]
+}
+ EOF
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+    sid       = "CreateCloudWatchLogs"
+  }
+}
+
+resource "aws_iam_policy" "lambda" {
+  name   = "${local.prefix}-lambda-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.lambda.json
+}
+
+resource "aws_lambda_function" "lambda" {
+  depends_on = [
+    null_resource.ecr_image
+  ]
+  function_name = "${local.prefix}-lambda"
+  role          = aws_iam_role.lambda.arn
+  timeout       = 300
+  image_uri     = "${aws_ecr_repository.lambda_image.repository_url}@${data.aws_ecr_image.lambda_image.id}"
+  package_type  = "Image"
+  environment {
+    variables = {
+      API_KEY             = jsondecode(data.aws_secretsmanager_secret_version.current_twitter_secrets.secret_string)["API_KEY"]
+      API_SECRET          = jsondecode(data.aws_secretsmanager_secret_version.current_twitter_secrets.secret_string)["API_SECRET"]
+      ACCESS_TOKEN        = jsondecode(data.aws_secretsmanager_secret_version.current_twitter_secrets.secret_string)["ACCESS_TOKEN"]
+      ACCESS_TOKEN_SECRET = jsondecode(data.aws_secretsmanager_secret_version.current_twitter_secrets.secret_string)["ACCESS_TOKEN_SECRET"]
+    }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "every_x_minutes" {
+  name                = "${local.prefix}-event-rule-lambda"
+  description         = "Fires every 15 minutes"
+  schedule_expression = "cron(0/20 * * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger_every_x_minutes" {
+  rule      = aws_cloudwatch_event_rule.every_x_minutes.name
+  target_id = "lambda"
+  arn       = aws_lambda_function.lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_x_minutes.arn
+}
